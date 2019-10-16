03a2e6093008361c206b57aa0ceade1cf58d5f72
dervis <dervisnurdag@gmail.com>
2019-10-16 15:37:21 +0300

    Kurumlar yapılıyor

---
+++ RandomTeam.Entities/Team.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomTeam.Entities
+{
+   public class Team
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+    }
+}
